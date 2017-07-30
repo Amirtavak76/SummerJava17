b8c1cd1357baad5b11fe2540780898e07b2dfc73
Amirtavak76 <Amirtavakoli76@gmail.com>
2017-07-30 13:09:16 -0700

    Contructor and toString created

--- Faz1/src/entities/Person.java
+++ Faz1/src/entities/Person.java
@@ -1,5 +1,36 @@
 package entities;
 
-public class Person {
+import java.util.Date;
 
+public class Person
+{
+	 private String _personName;
+	 private String _personHouseName;
+	 private String _personSchool;
+	 private String _personBloodStatus;
+	 private Date _personBirthDate;
+	 
+	 public Person()
+	 {
+		 this("", "", "", "", null);
+	 }
+	 public Person(String name)
+	 {
+		 this(name, " ", "", "", null);
+		 
+	 }
+	 public Person(String name, String houseName, String school, String bloodStatus, Date birthDate)
+	 {
+		 _personName = name;
+		 _personHouseName = houseName;
+		 _personSchool = school;
+		 _personBloodStatus = bloodStatus;
+		 _personBirthDate = birthDate;
+	 }
+	 public String toString()
+	 {
+		 String temp = new String();
+		 temp += _personName + '\n' + '\n' + _personHouseName + '\n' + _personSchool + '\n' +_personBloodStatus + '\n' + _personBirthDate;
+		 return temp;
+	 }
 }
